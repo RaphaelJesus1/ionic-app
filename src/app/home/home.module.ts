c63ce6540be70648f69ad3b4f08103e50700f04f
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2022-04-01 02:11:45 -0300

    home page component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [{ path: '', component: HomePage }];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [HomePage],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { University } from 'src/assets/interfaces/University';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  searchName: string;
+  searchCountry: string;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  setName(name: University['name']) {
+    if (this.searchName !== name) {
+      this.searchName = name;
+    }
+  }
+
+  setCountry(country: University['country']) {
+    if (this.searchCountry !== country) {
+      this.searchCountry = country;
+    }
+  }
+
+  sendRequest(): void {
+    const navigationExtras: NavigationExtras = {
+      queryParams: {
+        name: this.searchName,
+        country: this.searchCountry,
+      },
+    };
+
+    this.router.navigate(['university'], navigationExtras);
+  }
 }
